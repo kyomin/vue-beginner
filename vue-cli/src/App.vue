ca5409ca0d9bd555e50403b3a4ecdc0e4286ef5b
kyomin <kim031504@naver.com>
2021-05-08 15:51:29 +0900

    싱글 파일 컴포넌트에서 event emit 구현하기

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -5,7 +5,10 @@
       하위 컴포넌트에 props 전달 방식
       <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> 
     -->
-    <app-header v-bind:propsdata="str"></app-header>
+    <app-header 
+      v-bind:propsdata="str"
+      v-on:renew="renewStr"
+    ></app-header>
   </div>
 </template>
 
@@ -30,6 +33,11 @@ export default {
   },
   components: {
     'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function() {
+      this.str = 'Hi~~'
+    }
   }
 }
 </script>
